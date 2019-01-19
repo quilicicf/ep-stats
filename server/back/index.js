baf004288b89d311a3cadb952dacbf055fa8629d
quilicicf <cquilici@restlet.com>
2019-01-19 12:25:06 +0100

    :up: Start server from saved configuration

--- server/back/index.js
+++ server/back/index.js
@@ -2,25 +2,42 @@
 
 const { address } = require('ip');
 const express = require('express');
+const { resolve: resolvePath } = require('path');
 
+const readFile = require('../../lib/readFile');
+const writeFile = require('../../lib/writeFile');
 const addWarsResourcesToServer = require('./wars/addWarsResourcesToServer');
 const addTitansResourcesToServer = require('./titans/addTitansResourcesToServer');
 
-const [ sheetId, portOverwrite ] = process.argv.splice(2);
+const CONFIG_PATH = resolvePath(__dirname, 'config.json');
 
-if (!sheetId) {
-  process.stderr.write('You must give the sheet ID as first parameter\n');
-  process.exit(1);
-}
+const [ sheetIdOverwrite, portOverwrite ] = process.argv.splice(2);
 
-const DEFAULT_PORT = 12012;
+const main = async () => {
+  const { sheetId } = await readFile(CONFIG_PATH, JSON.parse, { sheetId: undefined });
 
-const app = express();
-const port = portOverwrite || DEFAULT_PORT;
-addWarsResourcesToServer(app, sheetId);
-addTitansResourcesToServer(app, sheetId);
+  if (!sheetId && !sheetIdOverwrite) {
+    process.stderr.write('You must give the sheet ID as first parameter for a first launch\n');
+    process.exit(1);
+  }
 
-app.listen(port, () => {
-  const baseUrl = `http://${address()}:${port}`;
-  process.stdout.write(`App running:\n  Wars: ${baseUrl}/wars\n  Titans: ${baseUrl}/titans\n`);
-});
+  if (!sheetId && sheetIdOverwrite) {
+    const config = { sheetId: sheetIdOverwrite };
+    await writeFile(CONFIG_PATH, JSON.stringify(config, null, 2));
+  }
+
+  const DEFAULT_PORT = 12012;
+
+  const app = express();
+  const port = portOverwrite || DEFAULT_PORT;
+  const sheetIdToUse = sheetIdOverwrite || sheetId;
+  addWarsResourcesToServer(app, sheetIdToUse);
+  addTitansResourcesToServer(app, sheetIdToUse);
+
+  app.listen(port, () => {
+    const baseUrl = `http://${address()}:${port}`;
+    process.stdout.write(`App running:\n  Wars: ${baseUrl}/wars\n  Titans: ${baseUrl}/titans\n`);
+  });
+};
+
+main();
