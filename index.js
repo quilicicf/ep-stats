ab910888387ed57eac028971bd043b307650f822
quilicicf <cquilici@restlet.com>
2019-02-28 14:17:12 +0100

    :up: Handle dates better

--- index.js
+++ index.js
@@ -4,12 +4,15 @@ const _ = require('lodash');
 const { resolve: resolvePath } = require('path');
 
 const listMembers = require('./lib/ep/listMembers');
-const uploadScore = require('./lib/ep/uploadScore');
-const patchConfig = require('./lib/cli/patchConfig');
+const uploadScoreAndUpdateCache = require('./lib/ep/uploadScoreAndUpdateCache');
+
+const patchConfig = require('./lib/cli/config/patchConfig');
 const selectScreenshots = require('./lib/cli/selectScreenshots');
+
 const extractHeader = require('./lib/image-processing/extractHeader');
 const getImageProcessor = require('./lib/image-processing/getImageProcessor');
 
+const CACHE_PATH = resolvePath(__dirname, 'cache.json');
 const CONFIG_PATH = resolvePath(__dirname, 'config.json');
 
 const processScreenshot = async (selectedScreenshot, members) => {
@@ -23,7 +26,6 @@ const processScreenshot = async (selectedScreenshot, members) => {
 const main = async () => {
   const patchedConfig = await patchConfig(CONFIG_PATH);
   const selectedScreenshots = await selectScreenshots(patchedConfig);
-  console.log(JSON.stringify(selectedScreenshots, null, 2));
 
   const members = await listMembers(patchedConfig);
   const resultPromises = _.map(
@@ -33,7 +35,8 @@ const main = async () => {
 
   const items = await Promise.all(resultPromises);
   const result = _.reduce(items, (seed, item) => ({ ...seed, ...item }), {});
-  uploadScore(result, patchedConfig);
+
+  await uploadScoreAndUpdateCache(result, patchedConfig, CACHE_PATH);
   console.log(JSON.stringify(result, null, 2));
 };
 
