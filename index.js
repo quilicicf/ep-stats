1231ef1ac2c024a2b603393e41e5c236b0002e0d
quilicicf <quilicicf@gmail.com>
2019-09-18 14:48:38 +0200

    :new: Set member score to N/A when they are on holidays and scored 0

--- index.js
+++ index.js
@@ -2,51 +2,79 @@
 
 const _ = require('lodash');
 const { red, green } = require('chalk');
+const format = require('date-fns/format');
 const { resolve: resolvePath } = require('path');
+const { registerPrompt, prompt } = require('inquirer');
+registerPrompt('datetime', require('inquirer-datepicker-prompt'));
 
 const listMembers = require('./lib/ep/listMembers');
-const uploadScoreAndUpdateCache = require('./lib/ep/uploadScoreAndUpdateCache');
+const uploadScore = require('./lib/ep/uploadScore');
+const activityNextDayComputers = require('./lib/ep/activityNextDayComputers');
 
 const configureOcrUserWords = require('./lib/ocr/configureOcrUserWords');
 
+const getFromCache = require('./lib/cli/cache/getFromCache');
+const storeInCache = require('./lib/cli/cache/storeInCache');
 const patchConfig = require('./lib/cli/config/patchConfig');
-const selectScreenshots = require('./lib/cli/selectScreenshots');
+const selectScreenshot = require('./lib/cli/selectScreenshot');
+const { LAST_DATE } = require('./lib/cli/cache/cacheKeys');
+const { HITS, INFO } = require('./lib/cli/screenshotTypes');
 
 const extractHeader = require('./lib/image-processing/extractHeader');
-const getImageProcessor = require('./lib/image-processing/getImageProcessor');
+const getInfoImageProcessor = require('./lib/image-processing/getInfoImageProcessor');
+const { HITS_INFO } = require('./lib/image-processing/imageProcessors');
+
+const { DATE_FORMAT } = require('./lib/google/constants');
+const getEvents = require('./lib/google/gcal/getEvents');
 
 const CACHE_PATH = resolvePath(__dirname, 'cache.json');
 const CONFIG_PATH = resolvePath(__dirname, 'config.json');
 
-const processScreenshot = async (screenshot, members, appConfig) => {
+const processInfoScreenshot = async (screenshot, members, appConfig) => {
   const header = extractHeader({ screenshot, appConfig });
-  const { type, processor } = getImageProcessor(header);
+  const { processor } = getInfoImageProcessor(header);
+  return processor({ screenshot, members, appConfig });
+};
 
-  const result = await processor({ screenshot, members, appConfig });
-  return { [ type ]: result };
+const findEventDate = async (info, pathInCache) => {
+  const { [ LAST_DATE ]: lastDate } = await getFromCache(CACHE_PATH, pathInCache);
+
+  const newDate = activityNextDayComputers[ pathInCache ](lastDate);
+  const formattedNewDate = format(newDate, DATE_FORMAT);
+
+  const { answer } = await prompt({
+    type: 'datetime',
+    name: 'answer',
+    initial: newDate,
+    message: `The detected date is ${formattedNewDate}, is that OK?`,
+    format: [ 'mm', '_', 'dd' ],
+  });
+  return answer;
 };
 
 const main = async () => {
   const patchedConfig = await patchConfig(CONFIG_PATH);
-  const selectedScreenshots = await selectScreenshots(patchedConfig);
 
   const members = await listMembers(patchedConfig);
   await configureOcrUserWords(_.keys(members));
 
-  const resultPromise = _.reduce(
-    selectedScreenshots,
-    (promise, selectedScreenshot) => promise
-      .then(seed => (
-        processScreenshot(selectedScreenshot, members, patchedConfig)
-          .then(item => [ ...seed, item ])
-      )),
-    Promise.resolve([]),
-  );
+  const selectedInfoScreenshot = await selectScreenshot(patchedConfig, INFO);
+  const info = await processInfoScreenshot(selectedInfoScreenshot, members, patchedConfig);
+  const { activityType: pathInCache } = info;
+  const eventDate = await findEventDate(info, pathInCache);
+  const formattedEventDate = format(eventDate, DATE_FORMAT);
 
-  const items = await resultPromise;
-  const result = _.reduce(items, (seed, item) => ({ ...seed, ...item }), {});
+  const holidays = await getEvents(patchedConfig);
 
-  await uploadScoreAndUpdateCache(result, patchedConfig, CACHE_PATH);
+  const selectedHitsScreenshot = await selectScreenshot(patchedConfig, HITS, selectedInfoScreenshot.imageNameWithExtension);
+  const hits = await HITS_INFO.processor({
+    screenshot: selectedHitsScreenshot, members, holidays, date: eventDate,
+  });
+
+  await uploadScore(info, hits, patchedConfig, formattedEventDate);
+
+  const newCacheEntry = { [ LAST_DATE ]: formattedEventDate };
+  await storeInCache(CACHE_PATH, pathInCache, newCacheEntry);
 };
 
 try {
