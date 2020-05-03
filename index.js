26a7f2e3df7ff80e7933d7667f5d7d4fce052ffa
quilicicf <quilicicf@gmail.com>
2020-05-03 16:10:00 +0200

    :new: Add initialize command (#4)

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 const yargs = require('yargs');
 
 const runCommand = require('./lib/commands/run');
+const initializeCommand = require('./lib/commands/initialize');
 const masterCommandName = require('./lib/masterCommandName');
 
 /* eslint-disable no-unused-expressions */
@@ -10,8 +11,10 @@ const masterCommandName = require('./lib/masterCommandName');
 yargs.usage(`USAGE: ${masterCommandName} <command> [options]`)
 
   .command(runCommand)
+  .command(initializeCommand)
 
   .help()
+  .strict(true)
   .wrap(null)
   .version()
   .argv;
