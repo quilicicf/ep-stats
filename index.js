dd4e374421cb38257bbba717ed0c8b2712868d28
quilicicf <quilicicf@gmail.com>
2020-05-14 18:01:20 +0200

    :triangular_ruler: Always execute in ep-stats configuration folder so OCR trained data is put there

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const runCommand = require('./lib/commands/run');
 const initializeCommand = require('./lib/commands/initialize');
 const masterCommandName = require('./lib/masterCommandName');
 
+const { EP_STATS_CONFIG_DIR_PATH } = require('./lib/conf/constants');
+
+process.chdir(EP_STATS_CONFIG_DIR_PATH); // Execute in the configuration path so trained data files are put there
+
 /* eslint-disable no-unused-expressions */
 // noinspection BadExpressionStatementJS
 yargs.usage(`USAGE: ${masterCommandName} <command> [options]`)
